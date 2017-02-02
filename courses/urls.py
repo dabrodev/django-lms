344a7208869cabe5903817bca8fe5b767ec9a006
Damian Dabrowski <dabdamian@gmail.com>
2017-02-02 19:50:15 +0100

    added urls

--- courses/urls.py
+++ courses/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'courses'
 urlpatterns = [
 	url(r'^$', views.course_list, name='list'),
-	url(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail, name='step_detail'),
-	url(r'(?P<pk>\d+)/$', views.course_detail, name='course_detail'),
+	url(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail, name='step'),
+	url(r'(?P<pk>\d+)/$', views.course_detail, name='detail'),
 ]
