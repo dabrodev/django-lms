51b650d4535a9beb5dadb267ea769d7b784b445e
Damian Dabrowski <dabdamian@gmail.com>
2017-02-03 20:31:28 +0100

    test added

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Course
 
 # Create your tests here.
+
+class CourseModelTests(TestCase):
+	'''Tests for the Course model'''
+	def test_course_creation(self):
+		course = Course.objects.create(
+			title="Python Regular Expressions", 
+			description="Learn to write regualar epressions with python"
+			)
+		now = timezone.now()
+		self.assertLess(course.created_at, now)
